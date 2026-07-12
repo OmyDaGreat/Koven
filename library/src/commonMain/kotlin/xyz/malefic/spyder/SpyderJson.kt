563d46e815ead441f3057efcce223555e299a61c
Om Gupta <ogupta4242@gmail.com>
2026-07-11 21:04:47 -0700

    feat(json): add ability to overwrite serializer module alternative for ppl used to it gave up on not requiring registering issues

--- library/src/commonMain/kotlin/xyz/malefic/spyder/SpyderJson.kt
+++ library/src/commonMain/kotlin/xyz/malefic/spyder/SpyderJson.kt
@@ -4,16 +4,41 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonBuilder
 import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.overwriteWith
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.serializer
 import kotlin.reflect.KClass
 
+/**
+ * Global JSON manager for the Spyder framework.
+ */
 object SpyderJson {
     private val issueSubclasses = mutableMapOf<KClass<out Issue>, KSerializer<out Issue>>()
+
+    /**
+     * Provide a custom module to overwrite the default one, for example to add custom Issue serializers.
+     *
+     * Usage example:
+     * ```
+     * val module = SerializersModule {
+     *     polymorphic(Issue::class) {
+     *         // Add custom issue serializers here
+     *         subclass(CustomIssue::class, CustomIssue.serializer())
+     *     }
+     * }
+     * ```
+     */
+    var module: SerializersModule? = null
+        set(value) {
+            field = value
+            default = build()
+        }
 
     init {
         registerIssue<InternalIssue>()
         registerIssue<BadRequestIssue>()
+        registerIssue<UnauthorizedIssue>()
+        registerIssue<RateLimitedIssue>()
     }
 
     var configuration: JsonBuilder.() -> Unit = {
@@ -35,7 +60,7 @@ object SpyderJson {
     /**
      * Registers a new [Issue] subclass.
      */
-    fun <T : Issue> registerIssue( // TODO: Find a way to not have to register issues
+    fun <T : Issue> registerIssue(
         clazz: KClass<T>,
         serializer: KSerializer<T>,
     ) {
@@ -60,6 +85,7 @@ object SpyderJson {
                         }
                     }
                 }
+            module?.let { serializersModule = serializersModule.overwriteWith(it) }
             block()
         }
 }
