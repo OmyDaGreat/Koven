8245f169ddbe13d9c5a3a992641501d7882d8763
Om Gupta <ogupta4242@gmail.com>
2026-09-21 14:53:51 -0400

    feat(ws): add registry
    optional process-local registry implementation for websockets

---
+++ library/src/jvmMain/kotlin/xyz/malefic/koven/server/WebSocketRegistry.kt
@@ -0,0 +1,58 @@
+package xyz.malefic.koven.server
+
+import org.http4k.websocket.Websocket
+import org.http4k.websocket.WsMessage
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * A registry for managing WebSocket connections in different rooms.
+ *
+ * Since this registry implementation is managed completely through memory, it is not suitable for use in a distributed environment.
+ *
+ * @param Room The type representing a room.
+ * @param Event The type representing an event to be broadcasted.
+ * @property encode A function to encode an event into a string message.
+ */
+class WebSocketRegistry<in Room : Any, in Event>(
+    private val encode: (Event) -> String,
+) {
+    private val rooms = ConcurrentHashMap<Room, MutableSet<Websocket>>()
+
+    fun join(
+        room: Room,
+        socket: Websocket,
+    ) {
+        rooms.computeIfAbsent(room) { ConcurrentHashMap.newKeySet() }.add(socket)
+    }
+
+    fun leave(
+        room: Room,
+        socket: Websocket,
+    ) {
+        rooms[room]?.remove(socket)
+        rooms.remove(room, emptySet())
+    }
+
+    fun broadcast(
+        room: Room,
+        event: Event,
+    ) {
+        val message = WsMessage(encode(event))
+
+        rooms[room]
+            ?.toList()
+            ?.forEach { socket ->
+                runCatching {
+                    socket.send(message)
+                }.onFailure {
+                    leave(room, socket)
+                }
+            }
+    }
+
+    fun closeRoom(room: Room) {
+        rooms.remove(room)?.forEach { socket ->
+            runCatching { socket.close() }
+        }
+    }
+}
